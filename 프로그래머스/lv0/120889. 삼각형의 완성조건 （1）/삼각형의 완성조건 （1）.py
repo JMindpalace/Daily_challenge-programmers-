0a0eacf9644cc16eed7c670691c70fc8cbd5fdc2
Kim Jae Hui <88294272+JMindpalace@users.noreply.github.com>
2022-12-20 18:19:16 +0900

    간결 1줄 추가

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,3 +1,5 @@
 def solution(sides):
     answer = max(sides)-(sides.pop(sides.index(min(sides)))+sides.pop(sides.index(min(sides))))
-    return 1 if answer<0 else 2  
+    return 1 if answer<0 else 2
+
+# 1 if max(sides) < (sum(sides) - max(sides)) else 2
