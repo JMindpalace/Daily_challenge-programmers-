2c3024ac19ffe54369f4c5e7180297eafa320b6b
Kim Jae Hui <88294272+JMindpalace@users.noreply.github.com>
2022-12-21 17:44:48 +0900

    람다와 count 사용 추가

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,2 +1,5 @@
 def solution(order):
     return sum([ 1 for i in str(order) if ( (int(i)%3==0) and (int(i)>0)) ])
+
+# sum(map(lambda x: str(order).count(str(x)), [3, 6, 9]))
+# order.count('3') + order.count('6') + order.count('9')
