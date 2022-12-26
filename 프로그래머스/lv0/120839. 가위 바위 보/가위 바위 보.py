f8c18b830831de41e1e0f635737ac35cc5d2ad64
Kim Jae Hui <88294272+JMindpalace@users.noreply.github.com>
2022-12-26 19:10:51 +0900

    딕셔너리 방법 추가

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,2 +1,6 @@
 def solution(rsp):
     return ''.join([ str(0) if int(i)==2 else ( str(5) if int(i)==0 else str(2)) for i in rsp ])
+
+# 딕셔너리 방법 추가
+# d = {'0':'5','2':'0','5':'2'}
+# return ''.join(d[i] for i in rsp)
