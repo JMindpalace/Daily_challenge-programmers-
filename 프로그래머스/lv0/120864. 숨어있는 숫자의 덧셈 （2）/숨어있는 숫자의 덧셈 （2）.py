c4a43b723ed83c9cd0bd227c6dc0f1f9f05da5c0
Kim Jae Hui <88294272+JMindpalace@users.noreply.github.com>
2022-12-26 18:28:21 +0900

    정규식 미사용 추가

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -3,3 +3,9 @@ import re
 def solution(my_string):
     digits = re.findall(r'\d+', my_string)
     return sum([int(i) for i in digits])
+
+# return sum([int(i) for i in re.findall(r'[0-9]+', my_string
+
+# 공백으로 구분후 합치기
+# s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+# return sum(int(i) for i in s.split())
